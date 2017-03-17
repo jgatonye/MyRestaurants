8d696793ff7858831102951258fe88a8f03f4e2d
jgatonye <gatonyejmaina@gmail.com>
2017-03-17 10:41:28 +0300

    Restaurant Application

---
+++ app/src/androidTest/java/com/maina/myrestaurants/RestaurantsActivityInstrumentationTest.java
@@ -0,0 +1,39 @@
+package com.maina.myrestaurants;
+
+/**
+ * Created by jgatonye on 3/14/17.
+ */
+import android.support.test.rule.ActivityTestRule;
+import android.view.View;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.core.IsNot.not;
+
+public class RestaurantsActivityInstrumentationTest {
+
+    @Rule
+    public ActivityTestRule<RestaurantsActivity> activityActivityTestRule =
+            new ActivityTestRule<RestaurantsActivity>(RestaurantsActivity.class);
+
+    @Test
+    public void listItemClickDisplaysToastWithCorrectRestaurant(){
+        View activityDecorView = activityActivityTestRule.getActivity().getWindow().getDecorView();
+        String restaurantName = "Artcaffe Karen";
+        onData(anything())
+                .inAdapterView(withId(R.id.listView))
+                .atPosition(0)
+                .perform(click());
+        onView(withText(restaurantName)).inRoot(withDecorView(not(activityDecorView)))
+                .check(matches(withText(restaurantName)));
+    }
+}
---
+++ app/src/main/java/com/maina/myrestaurants/MyRestaurantsArrayAdapter.java
@@ -0,0 +1,34 @@
+package com.maina.myrestaurants;
+
+//import ArrayAdapter;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by jgatonye on 3/15/17.
+ */
+
+public class MyRestaurantsArrayAdapter extends ArrayAdapter {
+    private Context mContext;
+    private String[] mRestaurants;
+    private String[] mCuisines;
+
+    public MyRestaurantsArrayAdapter(Context mContext, int resource, String[] mRestaurants, String[] mCuisines) {
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mRestaurants = mRestaurants;
+        this.mCuisines = mCuisines;
+    }
+    @Override
+    public Object getItem(int position){
+        String restaurant = mRestaurants[position];
+        String cuisine = mCuisines[position];
+        return String.format("%s \nServes great: %s", restaurant, cuisine);
+    }
+
+    @Override
+    public int getCount() {
+        return mRestaurants.length;
+    }
+}
